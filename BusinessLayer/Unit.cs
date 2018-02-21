1528ec9e27ee9a804fa260a74ef25bec96566f04
dimasrw34 <boyarskidm@yandex.ru>
2018-02-21 18:32:35 +0300

    fix)dnfversion

---
+++ BusinessLayer/Unit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Unit
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string ShortName { get; set; }
+    }
+}
--- DataLayer/Properties/AssemblyInfo.cs
+++ DataLayer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("TestDB")]
+[assembly: AssemblyTitle("DataLayer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestDB")]
+[assembly: AssemblyProduct("DataLayer")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("c977dcb9-6f47-40bc-9822-db00822e6592")]
+[assembly: Guid("da0e4821-33b7-4cd2-9b0d-5c94f831b473")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
